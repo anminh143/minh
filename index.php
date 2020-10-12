513a37700a6cd116959c170aa24eab83ac04af01
minh <anvanminh98@gmail.com>
2020-10-12 13:51:38 +0700

    calendar

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <!DOCTYPE html>
 <html>	
 <head>
-	<title>PHP</title>
+	<title>Tourist Attractions in Europe</title>
+	<style type="text/css">
+		.heading{
+			text-decoration: underline;
+			font-family: : Arial;
+			font-size: x-large;
+			font-weight: 200px;
+			text-transform: capitalize;
+		}
+		.general{
+			font-style: italic;
+			text-align: center;
+		}
+		.place{
+			text-decoration: underline;
+			font-family: cursive;
+			font-size: medium;
+			text-transform: capitalize;
+		}
+	</style>
 </head>
 <body>
 	<hr/>
